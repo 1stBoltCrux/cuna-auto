17672d73f57a45147795b67443af6a8d1fbbd259
Ted Leary <ted.e.leary@ey.com>
2020-12-13 17:05:28 -0800

    chore(formatting): format input styles

--- src/features/UI/input/Input.styles.tsx
+++ src/features/UI/input/Input.styles.tsx
@@ -1,29 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledInput = styled.div`
-    margin-bottom: 16px;
-   min-height: 90px;
- input {
+  margin-bottom: 16px;
+  min-height: 90px;
+  
+  input {
     margin-bottom: 4px;
     box-sizing: border-box;
-    border: 1px solid #BFC9CA;
+    border: 1px solid #bfc9ca;
     border-radius: 5px;
     padding: 8px;
     color: #424949;
     height: 40px;
     width: 100%;
- }
+  }
 
- label {
+  label {
     display: block;
     margin-bottom: 8px;
- }
+  }
 
- p {
+  p {
     margin: 0;
     color: red;
- }
+  }
 `;
-export {
-    StyledInput
-}
+export { StyledInput };
