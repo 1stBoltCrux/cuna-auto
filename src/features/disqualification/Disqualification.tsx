04fb56bdacf6607a9a07eff51d8bb21da8bf568b
Ted Leary <ted.e.leary@ey.com>
2020-12-13 16:27:37 -0800

    chore(formatting): formatting

--- src/features/disqualification/Disqualification.tsx
+++ src/features/disqualification/Disqualification.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { StyledDisqualificationContainer } from "./Disqualification.styles";
-import { selectDisqualifiedMessage } from '../landing/landingSlice';
+import { selectDisqualifiedMessage } from "../landing/landingSlice";
 
 const Disqualification = () => {
   const disqualificationMessage = useSelector(selectDisqualifiedMessage);
@@ -9,12 +9,15 @@ const Disqualification = () => {
   return (
     <StyledDisqualificationContainer>
       <div className="cuna-auto-loan-message-card">
-      <h3>Sorry, you're not qualified.</h3>
-      <p>{disqualificationMessage}</p>
-      <p>Please feel free to contact customer service at <a href="tel:555-555-5555">555-555-5555.</a></p>
+        <h3>Sorry, you're not qualified.</h3>
+        <p>{disqualificationMessage}</p>
+        <p>
+          Please feel free to contact customer service at{" "}
+          <a href="tel:555-555-5555">555-555-5555.</a>
+        </p>
       </div>
     </StyledDisqualificationContainer>
   );
-}
+};
 
 export default Disqualification;
